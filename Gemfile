c904aa027737c789ff8417ce42f81c3916fd61f3
Simon Coffey <simon@tribesports.com>
2013-03-20 12:04:13 +0000

    Setup: redcarpet/rouge highlighting, haml layout

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'nanoc', '~> 3.6.1'
+gem 'adsf'
+gem 'haml'
+gem 'redcarpet'
+gem 'rouge'
---
+++ lib/rouge.rb
@@ -0,0 +1,20 @@
+require 'redcarpet'
+require 'rouge'
+require 'rouge/plugins/redcarpet'
+
+class HighlitHTML < Redcarpet::Render::HTML
+  include Rouge::Plugins::Redcarpet
+end
+
+class HighlitMarkdownFilter < Nanoc::Filter
+  identifier :highlit_markdown
+  type :text
+
+  def run(content, options={})
+    renderer(options).render(content)
+  end
+
+  def renderer(options={})
+    Redcarpet::Markdown.new(HighlitHTML, options.merge(:fenced_code_blocks => true))
+  end
+end
