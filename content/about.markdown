286632d14778da32f64ccc0c24b41048b39477ea
Simon Coffey <simon@tribesports.com>
2013-09-02 10:39:52 +0100

    super-helpful 'about' section

---
+++ content/about.markdown
@@ -0,0 +1,9 @@
+---
+title: About
+---
+
+I'm Simon Coffey, a multi-purpose developer/engineer working mostly with
+Ruby these days at [Tribesports](http://tribesports.com).
+
+You can find me on [Twitter](http://twitter.com/urbanautomaton), or
+wherever it is I happen to be at the time you're looking.
--- lib/default.rb
+++ lib/default.rb
@@ -3,12 +3,11 @@
 
 def site_sections(current)
   [
-    ['home',            '/',                                   ('active' if current.identifier == '/')],
-    ['blog',            '/blog/',                              ('active' if current.identifier =~ /^\/blog/)],
-    # ['about',         '/about/',                             ('active' if current.identifier =~ /^\/about/)],
-    ['code',            'https://github.com/urbanautomaton',   nil],
-    ['twitter',         'https://twitter.com/urbanautomaton',  nil],
-    ['deadbadger.net',  'http://www.deadbadger.net',           nil],
-    # ['other',         '/other/',                             nil],
+    ['home',      '/',                                   ('active' if current.identifier == '/')],
+    ['blog',      '/blog/',                              ('active' if current.identifier =~ /^\/blog/)],
+    ['about',     '/about/',                             ('active' if current.identifier =~ /^\/about/)],
+    ['code',      'https://github.com/urbanautomaton',   nil],
+    ['tweets',    'https://twitter.com/urbanautomaton',  nil],
+    ['nonsense',  'http://www.deadbadger.net',           nil],
   ]
 end
