c9a4fc4061d4dfb00716f67b532e5d29047ed9ae
Simon Coffey <simon@tribesports.com>
2013-09-02 18:54:16 +0100

    Use typeup for blog typography

--- compass_config.rb
+++ compass_config.rb
@@ -1,9 +1,6 @@
 project_path = File.dirname(__FILE__)
-http_path = '/'
-output_style = :compressed
-sass_dir = 'content/style'
-css_dir = 'output/style'
-
-sass_options = {
-  :syntax => :scss
-}
+http_path    = '/'
+output_style = ENV['DEBUG'] ? :expanded : :compressed
+sass_dir     = 'content/style'
+css_dir      = 'output/style'
+sass_options = { :syntax => :scss }
--- lib/compass.rb
+++ lib/compass.rb
@@ -1,3 +1,5 @@
 require 'compass'
+require 'modular-scale'
+require 'typeup'
 
 Compass.add_project_configuration('compass_config.rb')
