d03f6300f9ca07fc634aca2206b35feea2604fbe
Simon Coffey <simon@urbanautomaton.com>
2017-01-29 14:06:47 +0000

    2D birds post

---
+++ content/blog/020-passable-2d-birds.markdown
@@ -0,0 +1,86 @@
+---
+kind: article
+title: Passable 2D Birds
+created_at: 2017-01-29T13:15:26+00:00
+comments: true
+draft: true
+categories: []
+---
+
+As previously mentioned, I'm trying to build a [bird flocking
+simulation](/blog/2017/01/19/birds-slowly/), initially based on the
+famous [boids](http://www.red3d.com/cwr/boids/) algorithm, and at first
+in 2D.
+
+I've managed to get this first step to a point I'm vaguely happy with.
+It looks like this:
+
+<script src="/toys/birds-001/vendor/underscore-min.js" type="text/javascript"></script>
+<script src="/toys/birds-001/vendor/sylvester.js" type="text/javascript"></script>
+<canvas id="canvas" width="600" height="600" style="border: 1px solid
+black;"></canvas>
+<button onclick="init();">Reset</button>
+<button onclick="toggleAnimation();">Pause/Play</button>
+<script src="/toys/birds-001/src/main.js" type="text/javascript"></script>
+
+I've implemented the main steering behaviours, and added a fourth,
+goal-seeking (the colors correspond to the marked vectors on one of the
+birds):
+
+* <span style="color: red;">*Separation*</span>: steer to avoid crowding local flockmates
+* <span style="color: green;">*Alignment*</span>: steer towards the average heading of local flockmates
+* <span style="color: black;">*Cohesion*</span>: steer to move toward the average position of local flockmates
+* <span style="color: blue;">*Goal-seeking*</span>: steer towards an arbitrary goal point
+
+To keep the birds on screen, and also to prevent them falling into a
+stable state, I've added some basic goal-seeking behaviour, in which
+the birds try to head towards a randomly-changing position (shown by the
+red dot).
+
+The pink circle around one of the birds approximately indicates its
+"neighbourhood", i.e. birds within this circle are considered its local
+flockmates. In practice there's a blind spot behind each bird, but I'm
+not good enough with the canvas API to draw this yet.
+
+## Thoughts
+
+I'm quite happy with this, but there's lots to improve upon.
+
+### Steering
+
+While the behaviours are all present, I'm not at all sure they're well
+balanced. In particular I don't think the separation behaviour has
+enough effect at close range, and I suspect that the alignment and
+goal-seeking behaviours are dominating. Without the goal-seeking, the
+birds have a tendency to drift off in small clumps, never to be seen
+again.
+
+The randomly changing goal is also responsible for pretty much all of
+the apparent dynamism of the system. Without this, the birds fall into a
+relatively stable pattern after a little while.
+
+I want to spend more time balancing these behaviours, and I think that's
+worth doing while it's still 2D, since the tweaks will (I hope)
+translate directly to the 3D version.
+
+### Flight model
+
+One thing that made a big difference to the "look" of the birds was to
+implement a maximum and minimum velocity, preventing the birds from
+stopping completely. This contributes greatly to the occasional "peels"
+of birds you see, since they have to make at least some sort of gradual
+turn to change direction, which then influences nearby birds.
+
+This is barely what you'd call a "flight model", though, so I'd like to
+improve it - specifically I'd like my birds to turn more swoopily[^1]. I
+think I'll leave this until I switch to 3D, though, since it's likely to
+be completely different. In 3D I'll have a better concept of the birds'
+orientation, so I'll be able to model different acceleration limits for
+different headings, roll, yaw rates and so forth.
+
+## Conclusion
+
+I'll let you know when I have one.
+
+[^1]:
+  This is an ornithological term.
---
+++ content/toys/birds-001/src/main.js
@@ -0,0 +1,261 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var X = canvas.width;
+var Y = canvas.height;
+var SCALE = 0.9;
+var BIRDS = 75;
+var ANIMATING = true;
+var ANIMATION_REQUEST_IDS = [];
+var MIN_VELOCITY = 40;
+var MAX_VELOCITY = 120;
+var NEIGHBOUR_RADIUS = 75;
+var VISIBLE_ANGLE = Math.PI * .8;
+var GOAL = $V([0, 0]);
+var GOAL_LIMIT = 150;
+
+var vel, pos, acc, last, visibility_matrix;
+
+var allRepels;
+var allHeadings;
+var allCentroids;
+var allGoals;
+
+function randPlusMinus(limit) {
+  return (Math.random() - 0.5) * limit * 2;
+}
+
+ctx.save();
+ctx.translate(X/2, Y/2);
+ctx.scale(SCALE, SCALE);
+
+function init() {
+  vel = [];
+  pos = [];
+  acc = [];
+  last = null;
+  visibility_matrix = new Array(BIRDS);
+
+  for (var i=0; i < BIRDS; i++) {
+    vel[i] = $V([randPlusMinus(100), randPlusMinus(100)]);
+    acc[i] = $V([0, 0]);
+    pos[i] = $V([randPlusMinus(200), randPlusMinus(200)]);
+    visibility_matrix[i] = new Array(BIRDS);
+  }
+}
+
+function pause() {
+  if (ANIMATING) {
+    ANIMATING = false;
+    for (var i=0; i<ANIMATION_REQUEST_IDS.length; i++) {
+      window.cancelAnimationFrame(ANIMATION_REQUEST_IDS[i]);
+    }
+  }
+}
+
+function play() {
+  if (!ANIMATING) {
+    ANIMATING = true;
+    last = null;
+    window.requestAnimationFrame(step);
+  }
+}
+
+function toggleAnimation() {
+  if (ANIMATING) {
+    pause();
+  } else {
+    play();
+  }
+}
+
+function drawCircle(centre, radius, color, stroke) {
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(centre.e(1), centre.e(2), radius, 0, Math.PI*2, true)
+  ctx.fill();
+  if (stroke) { ctx.stroke(); }
+}
+
+function drawTriangle(centre, heading, color, stroke) {
+  var deltas = [
+    $V([-1.0, 0]),
+    $V([0, 3.0]),
+    $V([1.0, 0]),
+    $V([0, 0])
+  ];
+  var angle = heading.angleFrom($V([0, 1])) * -Math.sign(heading.e(1));
+
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(centre.e(1), centre.e(2));
+  for (var i=0; i<deltas.length; i++) {
+    var point = centre.add(deltas[i].rotate(angle, $V([0, 0])).x(5));
+    ctx.lineTo(point.e(1), point.e(2));
+  }
+  ctx.fill();
+  if (stroke) { ctx.stroke(); }
+}
+
+function drawBird(bird) {
+  if (bird === 1) {
+    drawCircle(pos[bird], NEIGHBOUR_RADIUS, "rgba(255, 0, 0, 0.1)");
+    drawVector(pos[bird], allRepels[bird], "red");
+    drawVector(pos[bird], allHeadings[bird], "green");
+    drawVector(pos[bird], allCentroids[bird], "black");
+    drawVector(pos[bird], allGoals[bird], "blue");
+  }
+  drawTriangle(pos[bird], vel[bird], "green", true);
+}
+
+function drawVector(start, vector, color) {
+  var newPos = start.add(vector);
+  var originalStrokeStyle = ctx.strokeStyle;
+
+  ctx.strokeStyle = color || "black";
+  ctx.beginPath();
+  ctx.moveTo(start.e(1), start.e(2));
+  ctx.lineTo(newPos.e(1), newPos.e(2));
+  ctx.stroke();
+
+  ctx.strokeStyle = originalStrokeStyle;
+}
+
+function updateFrameRate(delta) {
+  var rate = 1 / delta;
+
+  var element = document.getElementById('frame-rate');
+  if (element) {
+    element.textContent = Math.round(rate * 100) / 100;
+  }
+}
+
+function sees(delta, velocity) {
+  return (
+    delta.modulus() <= NEIGHBOUR_RADIUS &&
+    velocity.angleFrom(delta) < VISIBLE_ANGLE
+  );
+}
+
+function repelVector(delta) {
+  return delta.toUnitVector().x(-1);
+}
+
+function sumVectors(vs) {
+  return _.reduce(
+    vs,
+    function(sum, el) { return sum.add(el); },
+    $V([0, 0])
+  );
+}
+
+function clamp(vector, min, max) {
+  var mod = vector.modulus();
+
+  if (mod > max) {
+    return vector.x(max / mod);
+  } else if (mod < min) {
+    return vector.x(min / mod);
+  } else {
+    return vector;
+  }
+}
+
+function goalSeeking(from) {
+  var heading = GOAL.subtract(from);;
+
+  return clamp(heading, 0, GOAL_LIMIT);
+}
+
+function updateAcceleration() {
+  allRepels = [];
+  allHeadings = [];
+  allCentroids = [];
+  allGoals = [];
+
+  for (var i=0; i<BIRDS; i++) {
+    var repel = $V([0, 0]);
+    var headings = [];
+    var centroids = [];
+
+    for (var j=0; j<BIRDS; j++) {
+      var iToj = pos[j].subtract(pos[i]);
+
+      if (sees(iToj, vel[i])) {
+        repel = repel.add(repelVector(iToj).x(15));
+        headings.push(vel[j]);
+        centroids.push(iToj);
+      }
+    }
+
+    var heading = sumVectors(headings).x(1/headings.length);
+    var centroid = sumVectors(centroids).x(1/centroids.length);
+    var goal = goalSeeking(pos[i]);
+
+    allRepels.push(repel);
+    allHeadings.push(heading);
+    allCentroids.push(centroid);
+    allGoals.push(goal);
+
+    acc[i] = repel.add(heading).add(centroid).add(goal);
+  }
+}
+
+function updateVelocity(delta_t) {
+  for (var i=0; i<BIRDS; i++) {
+    var v1 = vel[i].add(acc[i].x(delta_t));
+
+    vel[i] = clamp(v1, MIN_VELOCITY, MAX_VELOCITY);
+  }
+}
+
+function updatePosition(delta_t) {
+  for (var i=0; i<BIRDS; i++) {
+    pos[i] = pos[i].
+      add(vel[i].x(delta_t)).
+      add(acc[i].x(0.5 * delta_t * delta_t));
+  }
+}
+
+function drawBirds() {
+  for (var i=0; i<BIRDS; i++) { drawBird(i); }
+}
+
+function step(timestamp) {
+  if (!ANIMATING) { return; }
+  if (!last) { last = timestamp }
+  var delta_t = (timestamp - last) / 1000;
+  updateFrameRate(delta_t);
+  last = timestamp;
+
+  ctx.clearRect(-X/(2*SCALE), -Y/(2*SCALE), X/SCALE, Y/SCALE);
+
+  updateAcceleration();
+  updateVelocity(delta_t);
+  updatePosition(delta_t);
+  drawBirds();
+  drawCircle(GOAL, 3, "red", true);
+
+  if (ANIMATING) {
+    ANIMATION_REQUEST_IDS.push(window.requestAnimationFrame(step));
+  }
+}
+
+init();
+
+ANIMATION_REQUEST_IDS.push(window.requestAnimationFrame(step));
+
+function updateGoal() {
+  GOAL = $V([randPlusMinus(X/4), randPlusMinus(Y/4)]);
+  window.setTimeout(updateGoal, 5000);
+}
+
+updateGoal();
+
+document.addEventListener('visibilitychange', function() {
+  if (document.visibilityState === 'hidden') {
+    pause();
+  } else {
+    play();
+  }
+});
